35813b61e5fe371e3e4236584ff98b03451d2c1c
keneversley <krazorfbx@gmail.com>
2022-02-23 11:14:34 -0800

    pastpresentfuture

--- app.js
+++ app.js
@@ -3,4 +3,21 @@ var currentDay = moment().format("dddd, MMMM Do");
 console.log(currentDay)
 $("#currentDay").text(currentDay) 
 
+$( ".row" ).each(function() {
+    var currentHour = moment().hour();
+    var rowHour = $( this ).attr("id");
+    console.log(currentHour, rowHour)
+// cheking to see if current hour is past, present, or future
+    if(currentHour > rowHour){
+        $( this ).children(".description").addClass( "past" );  
+    } else if(currentHour < rowHour){
+        $( this).children(".description").addClass("future"); 
+
+    } else if (currentHour == rowHour){
+        $( this).children(".description").addClass("presnt"); 
+    }
+  });
+
+  
+
 
