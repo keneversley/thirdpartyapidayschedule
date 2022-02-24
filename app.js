8ec5c9b44bf6fca298bfe147461699d542d85932
keneversley <krazorfbx@gmail.com>
2022-02-23 18:59:52 -0800

    SAVEBTN

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 //   Format Dates
 var currentDay = moment().format("dddd, MMMM Do");  
 console.log(currentDay)
-$("#currentDay").text(currentDay) 
+$("#currentDay").text(currentDay); 
+
+$(".saveBtn").on("click", function () {
+    //get nearby values.
+    console.log(this);
+    var text = $(this).siblings(".description").val(); // taken the change from the sibling html description attribute
+    var time = $(this).parent().attr("id"); // taken the change from the parent html id attribute
+
+    //set items in local storage.
+    localStorage.setItem(time, text);
+})
 
 $( ".row" ).each(function() {
     var currentHour = moment().hour();
@@ -13,10 +23,13 @@ $( ".row" ).each(function() {
     } else if(currentHour < rowHour){
         $( this).children(".description").addClass("future"); 
 
-    } else if (currentHour == rowHour){
+    } else  (currentHour == rowHour)
         $( this).children(".description").addClass("presnt"); 
-    }
+    
   });
+  
+   //set items in local storage.
+//    localStorage.setItem(currentHour, rowHour);
 
   
 
